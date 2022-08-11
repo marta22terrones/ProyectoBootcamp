36c473f11dc08dc60b1f6dc6df8f6be0de511af3
mterrones <marta22terrones@gmail.com>
2022-08-11 18:22:06 +0200

    atributos, getter, setter y constructores en User para ir probando login

--- src/main/java/com/proyecto/entities/User.java
+++ src/main/java/com/proyecto/entities/User.java
@@ -2,6 +2,11 @@ package com.proyecto.entities;
 
 import java.time.LocalDate;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 /* 
  * Además de username, password y avatar posibilidad de añadir: dateBirth, genre y rol 
  * Rol: (desplegable de roles: ADMIN O NORMAL, que podemos usar más adelante con Spring Security)
@@ -11,6 +16,10 @@ import java.time.LocalDate;
   * He añadido los atributos y Getter, Setter y Constructores porque quiero ir empezando a ver el Login (Marta)
   */
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
     
     private long id;
