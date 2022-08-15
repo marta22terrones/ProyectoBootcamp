1f8fafb4dde54e9db2d16bc3d98762818337d0e5
mterrones <marta22terrones@gmail.com>
2022-08-15 11:35:11 +0200

    Branch entities finished

--- src/main/java/com/proyecto/entities/User.java
+++ src/main/java/com/proyecto/entities/User.java
@@ -1,40 +1,42 @@
 package com.proyecto.entities;
 
-import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-/* 
- * Además de username, password y avatar posibilidad de añadir: dateBirth, genre y rol 
- * Rol: (desplegable de roles: ADMIN O NORMAL, que podemos usar más adelante con Spring Security)
- */
-
- /*
-  * He añadido los atributos y Getter, Setter y Constructores porque quiero ir empezando a ver el Login (Marta)
-  */
-
-@Getter
-@Setter
-@AllArgsConstructor
+@Entity
+@Data
 @NoArgsConstructor
+@AllArgsConstructor
+//@Builder
 public class User {
-    
-    private long id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotEmpty(message = "The username cannot be empty")
+    @Size(min = 4, max = 20, message = "The username must have between 4 and 20 characters")
     private String username;
+
+    @NotEmpty(message = "The password cannot be empty")
+    @Size(min = 4, max = 20, message = "The password must have between 4 and 20 characters")
     private String password;
-    private LocalDate dateBirth;
-    private Genre genre;
-    private Rol rol;
+    
     private String avatar;
 
-    public enum Genre {
-        MAN, WOMAN, ANOTHER 
-    }
-
-    public enum Rol {
-        ADMIN, NORMAL
-    }
+    // @ManyToMany(mappedBy = "ratings")
+    // private List<Integer> ratings = new ArrayList<Integer>();
 }
