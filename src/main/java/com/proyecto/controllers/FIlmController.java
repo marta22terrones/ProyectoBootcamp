440587ff9aa060fe685f747160e57c4f9f05952f
Cristina <cristinaganfue@gmail.com>
2022-08-17 17:55:32 +0200

    Controlador inicial y html lista

--- src/main/java/com/proyecto/controllers/FIlmController.java
+++ src/main/java/com/proyecto/controllers/FIlmController.java
@@ -1,5 +1,29 @@
 package com.proyecto.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.proyecto.entities.Film;
+import com.proyecto.service.IFilmService;
+
+@Controller
+@RequestMapping("/")
 public class FilmController {
-    
+
+    @Autowired
+    private IFilmService filmService;
+
+    @GetMapping("/filmList")
+    public String getFilms(Model model) {
+
+        List<Film> films = filmService.getFilms();
+        model.addAttribute("filmsList", films);
+        
+        return "films";
+    }
 }
