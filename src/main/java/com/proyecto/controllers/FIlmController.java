bf2a46781b611b724abbad2482e971f77988d711
mterrones <marta22terrones@gmail.com>
2022-08-26 14:12:00 +0200

    back-lidia finished

--- src/main/java/com/proyecto/controllers/FIlmController.java
+++ src/main/java/com/proyecto/controllers/FIlmController.java
@@ -3,14 +3,14 @@ package com.proyecto.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.proyecto.entities.Actor;
+import com.proyecto.entities.Director;
 import com.proyecto.entities.Film;
 import com.proyecto.entities.Genre;
 import com.proyecto.service.IFilmService;
@@ -22,26 +22,20 @@ public class FIlmController {
     @Autowired
     private IFilmService filmService;
 
-    @GetMapping("/details/{id}")
+    @GetMapping("/catalogue/{id}")
     public ModelAndView details(@PathVariable(name = "id") int id){
         Film film = filmService.getFilm(id);
         List<Genre> gendres = filmService.getGendres(id);
+        List<Actor> actors = filmService.getActors(id);
+        List<Director> directors = filmService.getDirectors(id);
         ModelAndView mav = new ModelAndView();
         mav.setViewName("details");
         mav.addObject("film", film);
         mav.addObject("gendres", gendres);
+        mav.addObject("actors", actors);
+        mav.addObject("directors", directors);
         return mav;
     }
 
-    @GetMapping("/{title}")
-    public ResponseEntity<List<Film>> getFilmByString(@PathVariable(name = "string") String string) {
 
-        List<Film> films = filmService.getFilmByString(string);
-
-        if (films != null)
-            return new ResponseEntity<List<Film>>(films, HttpStatus.OK);
-        else
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-
-    }
 }
