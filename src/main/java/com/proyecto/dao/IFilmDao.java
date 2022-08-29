3cea5a99e73a91314e00b5eb8f271beef7560e51
mterrones <marta22terrones@gmail.com>
2022-08-29 09:58:23 +0200

    commit basicBack branch finished

--- src/main/java/com/proyecto/dao/IFilmDao.java
+++ src/main/java/com/proyecto/dao/IFilmDao.java
@@ -10,5 +10,7 @@ import com.proyecto.entities.Film;
 
 @Repository
 public interface IFilmDao extends JpaRepository<Film, Integer> {
-    
+
+    @Query(value = "select f from Film f where f.title like %:string%")
+    public List<Film> getFilmByString(String string);
 }
