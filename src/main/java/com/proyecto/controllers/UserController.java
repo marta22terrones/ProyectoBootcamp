64a3ecbaadcc09b5222c5f4338a120234cf3c874
rsanche4 <raquelus8@gmail.com>
2022-08-30 12:47:19 +0200

    barra de navegacion user

--- src/main/java/com/proyecto/controllers/UserController.java
+++ src/main/java/com/proyecto/controllers/UserController.java
@@ -1,5 +1,10 @@
 package com.proyecto.controllers;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.Date;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,8 +12,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.proyecto.entities.User;
@@ -21,26 +29,33 @@ public class UserController {
     @Autowired
     private IUserService userService;
 
-    @GetMapping("/login")
-    public ModelAndView loginUser(ModelMap map){
-        ModelAndView mav = new ModelAndView();
-
-        List<String> genres = new ArrayList<>();
-        genres.add(User.Genre.MAN.toString());
-        genres.add(User.Genre.WOMAN.toString());
-        genres.add(User.Genre.OTHER.toString());
-
-        map.addAttribute("genres", genres);
-
-        mav.setViewName("loginUser");   // * Nombre del HTML donde se encuentra el Login
-        return mav;
+    @GetMapping("/signUp")
+    public String loginUser(ModelMap map){  
+        User user = new User();
+        map.addAttribute("user", user);
+        return "signUp";
     }
 
     @PostMapping("/createUser")
-    public ModelAndView createUser() {
-        ModelAndView mav = new ModelAndView();
+    public String createUser(@ModelAttribute(name="user") User user,
+                                    @RequestParam(name = "imagen", required = false) MultipartFile file) {
 
+        if (!file.isEmpty()) {
+            String rutaAbsoluta = "C://Users/rsanche4/Documents/recursos/";
+            Path rutaCompleta = Paths.get(rutaAbsoluta + "//" + file.getOriginalFilename());       
+    
+            try {
+                byte [] bytesImages = file.getBytes();
+                Files.write(rutaCompleta, bytesImages);
 
-        return mav;
+                user.setAvatar(file.getOriginalFilename());
+
+                userService.saveUser(user);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }        
+            return "redirect:/catalogue";                                  
     }
 }
--- src/main/java/com/proyecto/entities/User.java
+++ src/main/java/com/proyecto/entities/User.java
@@ -10,18 +10,22 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
-@Data
 @NoArgsConstructor
 @AllArgsConstructor
-//@Builder
+@Builder
+@Getter
+@Setter
 public class User {
 
     @Id
@@ -29,30 +33,21 @@ public class User {
     private int id;
 
     @NotEmpty(message = "The username cannot be empty")
-    @Size(min = 4, max = 20, message = "The username must have between 4 and 20 characters")
+    // @Size(min = 4, max = 20, message = "The username must have between 4 and 20 characters")
     private String username;
 
     @NotEmpty(message = "The password cannot be empty")
-    @Size(min = 4, max = 20, message = "The password must have between 4 and 20 characters")
+    // @Size(min = 4, max = 20, message = "The password must have between 4 and 20 characters")
     private String password;
+    private String matchingPassword;
 
-    @NotEmpty(message = "The date of birth cannot be empty")
-    private LocalDate dateBirth;
-
-    private Genre genre;
-
-    private Rol rol;
+ 
+    private String email;
     
     private String avatar;
 
-    // @ManyToMany(mappedBy = "ratings")
+   //  @ManyToMany(mappedBy = "ratings")
     // private List<Integer> ratings = new ArrayList<Integer>();
 
-    public enum Genre {
-        MAN, WOMAN, OTHER
-    }
 
-    public enum Rol {
-        ADMIN, NORMAL
-    }
 }
